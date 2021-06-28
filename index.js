8c8cd5f8d1402d7619977a957b571df12c5a253b
Miten Gajjar <miten3377@gmail.com>
2021-06-28 21:14:29 +0530

    ex0

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const input = require("readline-sync");
-let score = 0;
+// const input = require("readline-sync");
+// let score = 0;
 
-const name = input.question("What is your name?\n");
+// const name = input.question("What is your name?\n");
 
-console.log("Hello! " + name + ", We are welcoming you here.");
+// console.log("Hello! " + name + ", We are welcoming you here.");
 
-const ans = input.question("Is my age is greater than 25?");
+// const ans = input.question("Is my age is greater than 25?");
 
-if (ans === "yes") {
-    console.log("You are right.");
-    score++;
-    console.log("score: " + score);
-} else {
-    console.log("You are wrong.");
+// if (ans === "yes") {
+//     console.log("You are right.");
+//     score++;
+//     console.log("score: " + score);
+// } else {
+//     console.log("You are wrong.");
+// }
+
+function add(first, second) {
+    return first + second;
 }
+
+console.log(add(5, 6));
