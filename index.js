32828d0d78457bfceabb3193af9be2c4d8af55af
Miten Gajjar <miten3377@gmail.com>
2021-06-28 21:06:31 +0530

    ex05

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const input = require("readline-sync");
 
 const name = input.question("What is your name?\n");
 
-console.log("Hello! "+name+", We are welcoming you here.")
+console.log("Hello! " + name + ", We are welcoming you here.");
+
+const ans = input.question("Is my age is greater than 25?");
+
+if (ans === "yes") {
+    console.log("You are right.");
+} else {
+    console.log("You are wrong.");
+}
+
